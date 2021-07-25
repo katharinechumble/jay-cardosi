4f434b5c7baaa930800e030b7c10f0cc5838a1d7
Katharine C Humble <katharinechumble@gmail.com>
2021-07-25 12:16:25 -0400

    in which I admit defeat

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # jay-cardosi
+
+## Technologies Used
+- HTML, CSS, Javascript
+- Bootstrap
+- JQuery
+- Open Weathermaps API
+
+## Description
+Ostensibly this app should allow a user to input the name of a city in order to find the current weather conditions and 5 day forecast for that city.  Furthermore, this app should also allow users to save past searches to Local Storage and display those past searches as buttons.  For whatever reason, the browser can't convert the city name into the requisite latitude and longitude coordinates and so the function returns an error.
+
+## Link
+https://katharinechumble.github.io/jay-cardosi/
+
+<img src=./assets/screenshot.png>
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,10 @@ var searchFormEl = document.querySelector("#search");
 var searchInputEl = document.querySelector("#search-box-form");
 var searchButtonEl = document.querySelector("#btn");
 var weatherContainerEl = document.querySelector("#present-container");
+var futureContainerEl = document.querySelector("#future-container");
 var pastSearchesEl = document.querySelector("#search-container");
+var cityName = searchInputEl;
+
 
 //function to prevent invalid searches
 var formCitySearch = function (event) {
@@ -12,21 +15,49 @@ var formCitySearch = function (event) {
   event.preventDefault();
 
   var cityName = searchInputEl;
+  //let lat = response.data.coord.lat;
+  //let lon = response.data.coord.lon;
 
   if (cityName) {
     getCityWeather(cityName);
-    searchInputEl.value = "";
-  } else {
-    alert("Please enter a valid city name.");
-  }
-  console.log(event);
+   searchInputEl.value = "";
+ } //else {
+ //   alert("Please enter a valid city name.");
+ // }
+ // console.log(event);
 };
 
+//put saved cities into an array and save to local storage
+function saveCity(city) {
+  var placeObj= {
+    city: city
+  }
+  placeArray.push(placeObj);
+  localStorage.setItem('placeArray', JSON.stringify(placeArray));
+};
+
+//pull searches from local storage and set them onto page as buttons
+//localStorage.setItem()
+function makeButtons() {
+  var placeArray = [];
+  for (var i=0; i < placeArray.length; i++) {
+    var button = $("<button>");
+    button.text(placeArray[i].city);
+    $("#search-container").append(button);
+    $(button).setAttribute("style", "btn btn-secondary");
+  }
+}
+
 // event listener for click
+searchInputEl.addEventListener("submit", getCityWeather);
 searchInputEl.addEventListener("submit", formCitySearch);
+searchInputEl.addEventListener("submit", makeButtons);
+//searchButtonEl.addEventListener("submit", getForecast);
 // function to pull current weather
-var getCityWeather = function (lat, lon) {
-  var presentContainer = document.getElementById("present-container");
+var getCityWeather = function (response) {
+  var presentContainer = document.getElementById("#present-container");
+  let lat = response.data.coord.lat;
+  let lon = response.data.coord.lon;
   console.log(lat + "" + lon);
   // format api URL
   var callApi =
@@ -35,44 +66,69 @@ var getCityWeather = function (lat, lon) {
     "&lon=" +
     lon +
     "&units=imperial" +
-    "&appid=imperial&appid=0b83383bcfd7067d3c959dbf82b86dc0";
+    "&appid=0b83383bcfd7067d3c959dbf82b86dc0";
   fetch(callApi)
     .then(response => {
       return response.json()
       .then(post => {
           console.log(post.current.weather[0]);
-          document.getElementById
-      })
+          document.getElementById('air-temp').textContent = Math.round(post.current.temp);
+          document.getElementById('wind-speed').textContent = "Wind speeds of" + post.current.wind_speed;
+          document.getElementById('current-conditions').textContent = post.current.weather[0].description;
+          document.getElementById('uv-index').textContent = post.current.uvi;
+
+          // future forecasts
+          document.getElementById('day-1').textContent = Math.round(post.daily[0].temp.day);
+          document.getElementById('day-2').textContent = Math.round(post.daily[1].temp.day);
+          document.getElementById('day-3').textContent = Math.round(post.daily[2].temp.day);
+          document.getElementById('day-4').textContent = Math.round(post.daily[3].temp.day);
+          document.getElementById('day-5').textContent = Math.round(post.daily[4].temp.day);
+          // future condition
+          document.getElementById('#day-1-conditions').textContent = post.daily[0].weather[0].main;
+          document.getElementById('#day-2-conditions').textContent = post.daily[1].weather[0].main;
+          document.getElementById('#day-3-conditions').textContent = post.daily[2].weather[0].main;
+          document.getElementById('#day-4-conditions').textContent = post.daily[3].weather[0].main;
+          document.getElementById('#day-5-conditions').textContent = post.daily[4].weather[0].main;
+          // humidity forecast
+          document.getElementById('day-1-humid').textContent = post.daily[0].humidity + "% humidity";
+          document.getElementById('day-2-humid').textContent = post.daily[1].humidity + "% humidity";
+          document.getElementById('day-3-humid').textContent = post.daily[2].humidity + "% humidity";
+          document.getElementById('day-4-humid').textContent = post.daily[3].humidity + "% humidity";
+          document.getElementById('day-5-humid').textContent = post.daily[4].humidity + "% humidity";
+          // wind forecast
+          document.getElementById('day-1-wind').textContent = post.daily[0].wind_speed;
+          document.getElementById('day-2-wind').textContent = post.daily[1].wind_speed;
+          document.getElementById('day-3-wind').textContent = post.daily[2].wind_speed;
+          document.getElementById('day-4-wind').textContent = post.daily[3].wind_speed;
+          document.getElementById('day-5-wind').textContent = post.daily[4].wind_speed;
+      });
     })
-    .catch(function (error) {
+    .catch(function (_error) {
       alert("Unable to connect to service.");
     });
 };
 
 // display weather for search value entered
-var displayWeather = function (data, current) {
-  var cityName = "Louisville";
-  weatherContainerEl.textContent = "";
-  var currentForecast = document.createElement("div");
-  currentForecast.classList = "flex-row justify-space-between align-center";
-  currentForecast.setAttribute("src", "./data/2.5/weather?q=" + cityName);
-};
+//var displayWeather = function (data, current) {
+  //var cityName = "Louisville";
+  //weatherContainerEl.textContent = "";
+  //var currentForecast = document.createElement("div");
+  //currentForecast.classList = "flex-row justify-space-between align-center";
+  //currentForecast.setAttribute("src", "./data/2.5/weather?q=" + cityName);
+//};
 
 // display future weather
-var getForecast = function (daily) {
-  var cityName = "Chicago";
-  fetch(
-    "https://api.openweathermap.org/data/2.5/forecast/daily?q=" +
-      cityName +
-      "&cnt=" +
-      5 +
-      "&appid=0b83383bcfd7067d3c959dbf82b86dc0"
-  ).then(function (response) {
-    response.json().then(function (data) {
-      displayForecast(data, daily);
-    });
-  });
-};
-
-//save searches to local storage
-//localStorage.setItem()
+//var getForecast = function (daily) {
+//  var cityName = "Chicago";
+  //fetch(
+    //"https://api.openweathermap.org/data/2.5/forecast/daily?q=" +
+      //cityName +
+    //  "&cnt=" +
+    //  5 +
+      //"&appid=0b83383bcfd7067d3c959dbf82b86dc0"
+//  ).then(function (response) {
+  //  response.json().then(function (data) {
+    //  displayForecast(data, daily);
+   // });
+//  });
+//};
